fab032ee6d232d884a0f6cb346623cb9c9fdf7a4
zachcaton <zachcaton@macbook-pro.fios-router.home>
2017-11-01 09:47:27 -0400

    Everything together now

---
+++ src/proj3/WarehouseManager.java
@@ -0,0 +1,8 @@
+package proj3;
+
+
+public class WarehouseManager extends Employee {
+    public WarehouseManager(String a, String b, String c, String d, String e) {
+        super(a, b, c, d, e);
+    }
+}
