deb6533c2d9f2c4f084a5f4dfdc6a11213fda500
zachcaton <zachcaton@macbook-pro.fios-router.home>
2017-11-01 09:52:32 -0400

    Everything together now

---
+++ src/proj3/SalesAssociate.java
@@ -0,0 +1,7 @@
+package proj3;
+
+public class SalesAssociate extends Employee {
+    public SalesAssociate(String a, String b, String c, String d, String e) {
+        super(a, b, c, d, e);
+    }
+}
